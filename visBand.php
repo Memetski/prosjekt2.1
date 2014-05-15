c88c82f51b77d9421b9b757509139436b5fb7d3b
Memetski <memet_cat@hotmail.com>
2014-05-15 13:45:08 +0200

    visBand er oppe å går

--- visBand.php
+++ visBand.php
@@ -5,14 +5,14 @@ require_once 'db.php';
 
 
 
-$sql = 'SELECT * FROM bands WHERE uid = ?';
+$sql = 'SELECT * FROM bands ORDER BY bname ASC';
 $sth = $db->prepare ($sql);
 $sth->execute (array ($_SESSION['uid']));
 
 echo "<h1>Her vil bandene som blir opprettet vises :)</h1>";
 
 while ($row = $sth->fetch()) {
-	echo "<a href='javascript:visBand({$row['bname']}></a>";
+	echo "<a href='javascript:visBand({$row['uid']});'>{$row['bname']}</a> </br>";
 	
 
 }
